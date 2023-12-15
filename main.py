7bebe1dda751203ef545bcebf8bb4932b9b8f39f
Charlie Brewis <109143750+charlie-brewis@users.noreply.github.com>
2023-12-15 17:14:13 +0000

    Added border color attribute to square

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ class Board:
 
         # Instantiate squares
 
+
         self.__win.getKey('x')
         self.__win.close()
+
+    def __create_board(self) -> list[list[Rectangle]]:
+        board = []
+
 
 
 class Square:
@@ -23,24 +28,27 @@ class Square:
                        top_left_y: int, 
                        square_size: int, 
                        chance_square_is_mine: float, 
-                       square_color: str) -> None:
+                       square_fill_color: str,
+                       square_border_color: str) -> None:
         
         self.__top_left_x = top_left_x
         self.__top_left_y = top_left_y
         self.__size = square_size
         self.__is_mine = self.__determine_if_square_is_mine(chance_square_is_mine)
-        self.__color = square_color
-        self.__square_graphical_object = self.__instantiate_square_graphical_object(self.__top_left_x, self.__top_left_y, self.__size, self.__color) 
+        self.__fill_color = square_fill_color
+        self.__border_color = square_border_color
+        self.__square_graphical_object = self.__instantiate_square_graphical_object(self.__top_left_x, self.__top_left_y, self.__size, self.__fill_color, self.__border_color) 
         self.draw()
 
     def __determine_if_square_is_mine(self, chance_square_is_mine: float) -> bool:
         return random.random() < chance_square_is_mine
     
-    def __instantiate_square_graphical_object(self, top_left_x: int, top_left_y: int, square_size: int, square_color: str) -> Rectangle:
+    def __instantiate_square_graphical_object(self, top_left_x: int, top_left_y: int, square_size: int, square_fill_color: str, square_border_color: str) -> Rectangle:
         top_left_point = Point(top_left_x, top_left_y)
         bottom_right_point = Point(top_left_x + square_size, top_left_y + square_size)
         square_graphical_object = Rectangle(top_left_point, bottom_right_point)
-        square_graphical_object.setFill(square_color)
+        square_graphical_object.setFill(square_fill_color)
+        square_graphical_object.setOutline(square_border_color)
         return square_graphical_object
     
     def draw(self) -> None:
@@ -55,7 +63,7 @@ class Square:
     def set_color(self, new_color: str) -> None:
         self.__square_graphical_object.setFill(new_color)
         self.draw()
-        
+
 
 def main() -> None:
     # Define board parameters
